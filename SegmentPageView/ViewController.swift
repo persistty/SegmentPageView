5edcbcfdac0d258f52b3f4dacf89a32db8f39211
persist <tangyong.liu@jimubox.com>
2016-05-13 10:18:43 +0800

    SegmentPageView commit

---
+++ SegmentPageView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SegmentPageView
+//
+//  Created by jm on 16/5/13.
+//  Copyright © 2016年 persist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
